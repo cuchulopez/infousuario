473ac713ea9c23bb28e07d8f6d3413366b3fa834
Javier Lopez <jvrelopez@gmail.com>
2021-12-23 09:42:07 -0300

    Agregando variables de entorno

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var cors = require('cors');
-// var infoUsuarioRouter = require('./routes/infoUsuario');
 var apiRouter = require('./routes/api');
 
 var app = express();
+require('dotenv').config();
+
 
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
@@ -21,22 +22,21 @@ app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', cors(), apiRouter);
-// app.use('/infoUsuario', infoUsuarioRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
