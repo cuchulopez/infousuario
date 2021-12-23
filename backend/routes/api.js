490b78db9ccf6bc76793c545c24cde2f284c013b
Javier Lopez <jvrelopez@gmail.com>
2021-12-23 09:40:50 -0300

    Agregando módulo dotenv

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -3,13 +3,18 @@ var router = express.Router();
 const { PowerShell } = require('node-powershell');
 
 router.get('/infoUsuario/:usuario', async function(req, res, next) {
-    let usuario = req.params.usuario;
+    const usuario = req.params.usuario;
+    const server = process.env.DC_SERVER;
+    const ouSearch = process.env.DC_OU_SEARCH;
+
     let ps = new PowerShell({
       executionPolicy: 'Bypass',
       noProfile: true
     });
+    
+    const printCommand = PowerShell.command`. ./../scripts/infoUsuario.ps1 ${usuario} ${server} ${ouSearch}`;
+
     // const printCommand = PowerShell.command`Get-ADUser -Identity ${usuario} -Properties * | Select Enabled,ObjectClass,CanonicalName,Description,Department,EmployeeID,EmailAddress,Title,@{Name="PasswordLastSet";Expression={[datetime]::FromFileTime($_.'PasswordLastSet')}} | ConvertTo-Json`;
-    const printCommand = PowerShell.command`. ./../scripts/infoUsuario.ps1 ${usuario}`;
     await ps.invoke(printCommand)
       .then(output => {
         console.log(output.raw);
